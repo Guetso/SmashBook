a5dbd70a24be8cb1d9cead048e1b9e263d222f3f
Guetso <legalloisprovensal@gmail.com>
2020-09-11 22:19:13 +0200

    add condition to findOrCreate participant

--- API/controllers/participant.js
+++ API/controllers/participant.js
@@ -5,9 +5,16 @@ exports.createParticipant = (req, res, next) => {
     where: { playerId: req.body.playerId, characterId: req.body.characterId }
   })
     .then(([character, created]) => {
-      res.status(201).json({
-        message: `Le participant a bien été inscrit !`
-      })
+      if (created) {
+        res.status(201).json({
+          message: `Le nouveau participant a bien été inscrit !`
+        })
+      }
+      if (!created) {
+        res.status(200).json({
+          message: 'Le participant a bien été inscrit !'
+        })
+      }
     })
     .catch((error) => {
       const errorMessage = null

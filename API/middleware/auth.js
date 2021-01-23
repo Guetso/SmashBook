e89e14d2c3b37979c95a3c3e835e904daa0fd5a9
Guetso <legalloisprovensal@gmail.com>
2021-01-23 22:54:58 +0100

    add playerid headers to auth request

--- API/middleware/auth.js
+++ API/middleware/auth.js
@@ -3,33 +3,46 @@ const jwtConfig = require('../config/auth.config') // On récupère la clé pour
 
 exports.adminAuth = (req, res, next) => {
   try {
+    const whoSasking = req.headers.playerid
     const token = req.headers.authorization.split(' ')[1] // Récupération du token dans l'entête
     const decodedToken = jwt.verify(token, jwtConfig.secret) // On vérifie le token avec la clé pour lire ce TOKEN
     const playerId = decodedToken.playerId // Le token devient un objet JS classique qu'on place dans une constante, et on y récupère l'user ID pour comparaison le cas échéant
     const isAdmin = decodedToken.isAdmin
-    if ((req.body.userId && req.body.userId !== playerId) || !isAdmin) {
-      throw "Vous n'avez pas l'autorisation !"
+    console.log(whoSasking, playerId)
+    if (!whoSasking) {
+      throw { error: 'Qui êtes vous ?' } // Vérifie si la requete contient un header "playerid"
+    }
+    if (whoSasking != playerId) {
+      throw { error: "Vous n'êtes pas celui qui vous prétendez être !" } // Vérifie que le token correspond à "playerid"
+    }
+    if (!isAdmin) {
+      throw { error: "Vous n'avez pas l'autorisation !" } // Vérifie que l'utilisateur est admin
     } else {
       next()
     }
   } catch (error) {
     console.log(error)
-    res.status(401).json({ error: error | 'Requête non authentifiée !' })
+    res.status(401).json(error || { error: 'Requête non authentifiée !' })
   }
 }
 
 exports.auth = (req, res, next) => {
   try {
+    const whoSasking = req.headers.playerid
     const token = req.headers.authorization.split(' ')[1] // Récupération du token dans l'entête
     const decodedToken = jwt.verify(token, jwtConfig.secret) // On vérifie le token avec la clé pour lire ce TOKEN
     const playerId = decodedToken.playerId // Le token devient un objet JS classique qu'on place dans une constante, et on y récupère l'user ID pour comparaison le cas échéant
-    if (req.body.userId && req.body.userId !== playerId) {
-      throw "Vous n'avez pas l'autorisation !"
+
+    if (!whoSasking) {
+      throw { error: 'Qui êtes vous ?' } // Vérifie si la requete contient un header "playerid"
+    }
+    if (whoSasking != playerId) {
+      throw { error: "Vous n'êtes pas celui qui vous prétendez être !" } // Vérifie que le token correspond à "playerid"
     } else {
       next()
     }
   } catch (error) {
     console.log(error)
-    res.status(401).json({ error: error | 'Requête non authentifiée !' })
+    res.status(401).json(error || { error: 'Requête non authentifiée !' })
   }
 }

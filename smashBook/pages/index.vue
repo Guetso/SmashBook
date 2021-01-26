9aef8e7ad2c8c815765564b2731309b181cc1f85
Guetso <legalloisprovensal@gmail.com>
2021-01-26 21:14:18 +0100

    ok logout

--- smashBook/pages/index.vue
+++ smashBook/pages/index.vue
@@ -5,6 +5,7 @@
     <input v-model="form.name" placeholder="user" />
     <input v-model="form.password" placeholder="mdp" />
     <button type="submit" @click="loginMe">Ok</button>
+    <button @click="logMeOut">Se déconnecter</button>
   </div>
 </template>
 
@@ -18,8 +19,18 @@ export default {
   methods: {
     loginMe() {
       this.$store.dispatch('auth/login', this.form).then(
+        () => {
+          console.log('Vous êtes connecté', response)
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+    },
+    logMeOut() {
+      this.$store.dispatch('auth/logout').then(
         (response) => {
-          console.log(response)
+          console.log('Vous êtes déconnecté')
         },
         (error) => {
           console.log(error)

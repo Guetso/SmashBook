8dc09d6f927b1bb8a1dfea37b03aec76eacc2a5f
Guetso <legalloisprovensal@gmail.com>
2021-02-26 23:18:45 +0100

    fix path to the image

--- API/datas/characters.js
+++ API/datas/characters.js
@@ -3,6 +3,6 @@ module.exports = [
     name: 'Mario',
     gameId: 1,
     from: 'Super Mario',
-    imageUrl: `${process.env.HOST}/images/charactersdHead/MarioHead.png}`,
+    imageUrl: `${process.env.HOST}/images/charactersdHead/MarioHead.png`,
   },
 ]

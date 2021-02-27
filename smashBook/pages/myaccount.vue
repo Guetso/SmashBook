71d98afa46c67323bae192b17daa0326709794ef
Guetso <legalloisprovensal@gmail.com>
2021-02-27 16:29:01 +0100

    Fix v is null

--- smashBook/pages/myaccount.vue
+++ smashBook/pages/myaccount.vue
@@ -101,7 +101,7 @@ export default {
       return {
         name: this.me.name,
         email: this.me.email,
-        bio: this.me.bio,
+        bio: this.me.bio || '',
         imageUrl: null,
       }
     },
